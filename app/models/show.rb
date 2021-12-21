11032406358651ea02425a39e64661521c233dfe
nluuarch@gmail.com <nluuarch@gmail.com>
2021-12-20 16:01:00 -0800

    commit tree d3bcd53450195a0ce6a92de857d09d1e5f840ea4

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list 
+    self.actors.map do |actor|
+        # "#{actor.first_name} #{actor.last_name}"
+        # uses the method from actor on the actor instance 
+        actor.full_name
+    end
+  end
+
 end
